3901e847cdaec6dd44690df703eca035197ca00c
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-04-18 16:46:25 +0530

    wrapped main file with BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import App2 from "./App2.jsx";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      {/* <App /> */}
+      <App2 />
+    </BrowserRouter>
+  </StrictMode>
+);
